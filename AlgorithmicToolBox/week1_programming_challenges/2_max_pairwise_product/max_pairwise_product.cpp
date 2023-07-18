b71a049a64141a4f1aa1a38cd252940380591165
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-07-19 01:00:00 +0500

    Maximum Pairwise Multiplication task

---
+++ AlgorithmicToolBox/week1_programming_challenges/2_max_pairwise_product/max_pairwise_product.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
+    int n = numbers.size();
+    int maximum = -1;
+    int secondMaximum = -2;
+
+    for (int first = 0; first < n; first++) {
+        if (numbers[first] >= maximum)
+        {
+            secondMaximum = maximum;
+            maximum = numbers[first];
+        }
+        else if (numbers[first] >= secondMaximum)
+        {
+            secondMaximum = numbers[first];
+        }
+        std::cout << first << std::endl;
+    }
+    max_product = maximum * secondMaximum;
+    return max_product;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
+    return 0;
+}
